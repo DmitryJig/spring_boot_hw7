34dc7327ba2937a291ddee20524f5ee6cdae573e
dima <jd2344430@gmail.com>
2022-11-10 11:46:35 +0500

    master commit

---
+++ src/test/java/com/example/spring_data_hw7/SpringDataHw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_hw7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataHw7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
